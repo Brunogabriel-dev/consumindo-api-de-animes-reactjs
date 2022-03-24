d7b3e75931de153e6618202c3525e3ed6a3434e0
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-23 22:44:25 -0300

    Inclusão de texto

--- src/useDebounce.js
+++ src/useDebounce.js
@@ -1,7 +1,14 @@
-export default function useDebounce(fn, delay) {
-  
-  function debouncedFn() {
+import { useRef } from "react";
 
+export default function useDebounce(fn, delay) {
+  const timeoutRef = useRef(null)
+  
+  function debouncedFn(...args) {
+    window.clearTimeout()
+    timeoutRef.current = window.setTimeout(() => {
+      fn(...args);
+    }, delay);
+    
   }
 
   return debouncedFn;
